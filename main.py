b4b57048ee41479265d8821b9917fab376378cd5
Jess <jessicarturner00@gmail.com>
2022-09-15 09:02:32 +0800

    -Updated main.py from an infinite loop to a main function and if name == main. -Fixed communication diagram numbering.

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from alice import Alice
 from bob import Bob
 from letterbox import Letterbox
 
-alice_letterbox = Letterbox()
-bob_letterbox = Letterbox()
-bob = Bob(False, alice_letterbox, bob_letterbox)
-alice = Alice(True, bob_letterbox, alice_letterbox)
 
-while True:
-    alice.check_letter_box()
+def main():
+    alice_letterbox = Letterbox()
+    bob_letterbox = Letterbox()
+    bob = Bob(False, alice_letterbox, bob_letterbox)
+    alice = Alice(True, bob_letterbox, alice_letterbox)
+
     bob.check_letter_box()
+    alice.check_letter_box()
 
 
+if __name__ == '__main__':
+    main()
